cfaf9a18af004a9d21ef46b2c1eeb82adb6a26e9
seastian <seastian@users.noreply.github.com>
2021-07-16 22:36:31 -0300

    setup mdx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto">
+      <Component {...pageProps} />
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
